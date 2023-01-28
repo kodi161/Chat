225bb2a2fb3f358344ebd15afbcfe9d18772c9b6
Kodi161 <elleonluis10@gmail.com>
2023-01-28 23:24:52 +0100

    a

--- Chat/com/modelo/HiloClienteComunicacion.java
+++ Chat/com/modelo/HiloClienteComunicacion.java
@@ -5,7 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class HiloClienteComunicacion extends Thread{
 	//Atributos Simples
